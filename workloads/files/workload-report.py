32179f543a1306e4d66b47a9203ff75140a39d46
yusuf_yak <yusuf_yak@live.com>
2020-11-12 13:18:39 -0500

    New Ansible playbook for generating reports
    * Fix stress workload job
    
    * change k8s job script
    
    * Revert changes to run.sh config
    
    * Test prometheus query/reports with pod vertical
    
    * New ansible playbook for generating reports
    
    Set workload starttime as Ansible task
    
    Add step variable
    
    run python report script
    
    fix start.end timestamp issue
    
    Set start and end time from orchestration host
    
    fix j2 template syntax
    
    Create new workload vars yml file
    
    get type
    
    convert to yaml
    
    This is a combination of 15 commits.
    
    add multi params
    
    Add new report generator playbook
    
    Add new vars file
    
    Debug vars
    
    Debug vars
    
    WIP
    
    WIP
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    This is a combination of 10 commits.
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    This is a combination of 10 commits.
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    * Cleanup debug messages and delete test files

---
+++ workloads/files/workload-report.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python3
+
+import subprocess
+import sys
+import os
+import json
+from time import time
+from datetime import datetime, timedelta
+import urllib3
+import argparse
+import csv
+import pandas as pd
+import plotly.express as px
+
+# supress InsecureRequestWarning message
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Import dependency
+try:
+    import requests
+    from requests.auth import HTTPBasicAuth
+except Exception as e:
+    exit()
+
+# parseMetrics
+def parseMetrics(metrics_dictionary, result_type):
+    metrics = []
+    counter=0
+    for row in (metrics_dictionary['data']['result']):
+        print("Metrics lenght: {}".format(len(metrics_dictionary['data']['result'])))
+        if result_type == 'vector':
+            counter = counter + 1
+            metric = round(float(row['value'][1]), 1)
+            metrics.append(str(counter) + ',' + str(row['value'][0]) + ',' + str(metric))
+        elif result_type == 'matrix':
+            print("Metrics matrix lenght: {}".format(len(row['values'])))
+            for value in row['values']:
+                counter = counter + 1
+                metric = round(float(value[1]), 1)
+                metrics.append(str(counter) + ',' + str(value[0]) + ',' + str(metric))
+    return metrics
+
+# create endpoint query
+def createEndpoint(query_params_dict):
+    query_params    = {}
+    auth_credentials = {}
+    # Range vector query
+    if query_params_dict.get('end') is not None:
+        query_params['query'] = query_params_dict.get('promql')
+        query_params['start'] = query_params_dict.get('start')
+        query_params['end']   = query_params_dict.get('end')
+        if query_params_dict.get('step') is not None:
+            query_params['step']  = query_params_dict.get('step')
+        endpoint = query_params_dict['url'] + '/api/v1/query_range'
+    # Instant vector query
+    else:
+        query_params['query'] = query_params_dict.get('promql')
+        query_params['time']  = query_params_dict.get('start')
+        endpoint = query_params_dict['url'] + '/api/v1/query'
+
+    # Basic auth params
+    if query_params_dict.get('user') is not None and query_params_dict.get('pass') is not None:
+        auth_credentials['user'] = query_params_dict.get('user')
+        auth_credentials['pass'] = query_params_dict.get('pass')
+
+    # Request timeout param 
+    if query_params_dict.get('timeout') is not None:
+            query_params['timeout'] = query_params_dict.get('timeout')
+    # return url , api query params and basic auth credentials
+    print(query_params)
+    return endpoint, query_params, auth_credentials
+
+# Send promql request
+def getMetrics(url, params, user, password):
+    try:
+        if user is not None and password is not None:
+            response = requests.get(url, auth=HTTPBasicAuth(user, password), verify=False, params=params)
+        else:
+            response = requests.get(url, verify=False, params=params)
+        print("HTTP status code: {}, message: {}, URL: {}".format(response.status_code, response.reason, response.request.url))
+        if response.status_code == 200:
+            data = json.loads(response.text)
+            if data['status'] != 'success': # check success or failure
+                exit()
+            if len(data['data']['result']) < 0: # check if resultset is empty
+                exit()
+            metrics = parseMetrics(data, data['data']['resultType'])
+            return metrics   
+        else:
+           print("Something went wrong, HTTP status code: {} - Message: {}/{}".format(response.status_code, response.reason, response.text))
+    except requests.exceptions.RequestException as re:
+        print(re)
+    #except requests.exceptions.ConnectionError as ce:
+    #    print(ce)
+    #except requests.exceptions.ConnectTimeout as ct:
+    #    print(ct)
+
+# Plot metrics graph from csv
+def generateGraphReport(reportFile, x_axis="SN", y_axis="METRIC_VALUE", title="", file_name=""):
+    df1 = pd.read_csv(reportFile)
+    fig1 = px.line(df1, x=x_axis, y=y_axis, title=title)
+    fig1.write_image(file_name)
+
+# File path to store csv output
+def writeToCsvFile(cmd_args, metrics):
+    file_path = cmd_args.get('file_path')
+    if os.path.exists(file_path) and os.path.isdir(file_path):
+        file_name = os.path.join(file_path, cmd_args.get('file_name'))
+        # write metrics to file
+        print("Writing metrics to csv file: {}".format(file_name))
+        with open(file_name, 'w') as csv_file:
+            fieldNames = ['SN', 'TIMESTAMP', 'METRIC_VALUE']
+            csvWriter = csv.DictWriter(csv_file, fieldnames=fieldNames)
+            csvWriter.writeheader() 
+            print("{},{}".format(fieldNames[0], fieldNames[1]))
+            for i in metrics:
+                row = i.split(',')
+                csvWriter.writerow({fieldNames[0]: row[0], fieldNames[1]: row[1], fieldNames[2]: row[2]})
+                #print("{},{},{}".format(row[0], row[1], row[2])) 
+        return file_name
+    else:
+        print("Path is not a directory and/or does not exist: {}".format(file_path))
+        return ""
+
+# Process command line args
+parser = argparse.ArgumentParser(description='Run Prometheus queries (promql) using promethues HTTP API.', epilog="Happy quering! :)")
+
+# Args. Checkout ansible's argparse
+parser.add_argument("--start",      help="Start timestamp, Defaults to time for instant vector time-series samples", required=True)
+parser.add_argument("--end",        help="End timestamp", required=False)
+parser.add_argument("--step",       help="Query resolution step width in duration format or float number of seconds", required=True)
+parser.add_argument("--interval",   help="Prometheus range vector interval", required=False)
+parser.add_argument("--promql",     help="Prometheus expression query string", required=True)
+parser.add_argument("--user",  help="Prometheus basic auth user", required=False)
+parser.add_argument("--pass",  help="Prometheus basic auth password", required=False)
+parser.add_argument("--url",   help="Prometheus server url", required=True)
+parser.add_argument("--timeout",  help="Prometheus HTTP request timeout", required=False)
+parser.add_argument("--file_path",  help="File path to store csv output", required=True)
+parser.add_argument("--file_name",  help="File name using workload/job name", required=True)
+parser.add_argument("--print",  help="Print", required=False)
+args = parser.parse_args()
+
+# Main
+if __name__ == '__main__':
+    # Get api url, http query params and auth credentials if exist
+    url, params, basic_auth = createEndpoint(vars(args))
+
+    # Send HTTP request and pull metrics from prometheus server
+    metrics = getMetrics(url, params, basic_auth.get('user'), basic_auth.get('pass'))
+
+    # Write metrics as csv data
+    metrics_csv_file = writeToCsvFile(vars(args), metrics)
+
+    # Generate images
+    print("CSV File: {}".format(metrics_csv_file))
+    if os.path.isfile(metrics_csv_file):
+        file_path = vars(args).get('file_path')
+        file_name = vars(args).get('file_name').split('.', 1)[0] + '-graph.png'
+        image_file = os.path.join(file_path, file_name)
+        print("Image File: {}".format(image_file))
+        generateGraphReport(metrics_csv_file, title=file_name, file_name=image_file)
