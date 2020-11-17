2f40c6c12df8ebb95a25ba383245949fa9894f7a
Yusuf Yakubu <yusuf_yak@live.com>
2020-11-17 10:50:14 -0500

    Add dimension/grouping for prometheus reports

--- workloads/files/workload-report.py
+++ workloads/files/workload-report.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import subprocess
 import sys
 import os
 import json
@@ -11,6 +10,9 @@ import argparse
 import csv
 import pandas as pd
 import plotly.express as px
+import datetime
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 # supress InsecureRequestWarning message
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -28,16 +30,22 @@ def parseMetrics(metrics_dictionary, result_type):
     counter=0
     for row in (metrics_dictionary['data']['result']):
         print("Metrics lenght: {}".format(len(metrics_dictionary['data']['result'])))
+        keys = list(row.get('metric').keys())
+        try:
+            metric_name = row['metric'].get(keys[0])
+        except IndexError:
+            metric_name = "n/a"
         if result_type == 'vector':
+            print(row)
             counter = counter + 1
             metric = round(float(row['value'][1]), 1)
-            metrics.append(str(counter) + ',' + str(row['value'][0]) + ',' + str(metric))
+            metrics.append(str(counter) + ',' + str(row['value'][0]) + ',' + str(metric) + "," + metric_name)
         elif result_type == 'matrix':
             print("Metrics matrix lenght: {}".format(len(row['values'])))
             for value in row['values']:
                 counter = counter + 1
                 metric = round(float(value[1]), 1)
-                metrics.append(str(counter) + ',' + str(value[0]) + ',' + str(metric))
+                metrics.append(str(counter) + ',' + str(value[0]) + ',' + str(metric) + ',' + metric_name)
     return metrics
 
 # create endpoint query
@@ -96,9 +104,80 @@ def getMetrics(url, params, user, password):
     #    print(ct)
 
 # Plot metrics graph from csv
-def generateGraphReport(reportFile, x_axis="SN", y_axis="METRIC_VALUE", title="", file_name=""):
+
+def plotGraph(csv_file, plot_title, x_label, y_label, image_file):
+    x_axis = []
+    y_axis = []
+    timestamp = []
+    data_set = {}
+
+    with open(csv_file, newline='') as csvfile:
+        data = csv.reader(csvfile, delimiter=',')
+        for row in data:
+            if row[0] == 'SN':
+                continue
+            # timestamps
+            if row[1] not in x_axis:
+                x_axis.append(row[1])
+            # metrics name/group rows
+            if row[3] not in y_axis:
+                y_axis.append(row[3])
+
+    for i in x_axis:
+        ts = datetime.datetime.fromtimestamp(int(i)).isoformat()
+        timestamp.append(str(ts).split('T')[1])
+    print(timestamp)
+
+    # Group metrics by dimension i
+    for i in y_axis:
+        data_set[i] = []
+
+    with open(csv_file, newline='') as csvfile:
+        data = csv.reader(csvfile, delimiter=',')
+        for row in data:
+            for y_axis_value in y_axis:
+                if y_axis_value == row[3]:
+                    tmp = data_set.get(y_axis_value)
+                    if row[2] != None and row[2] != 'nan' and row[2] != "":
+                        tmp.append(row[2]) 
+                    else:
+                        tmp.append(0.0)
+                    data_set[y_axis_value] = tmp
+
+    # Draw/plat chart
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    x_label = "Time <from: " + timestamp[0] + " to " + timestamp[-1] + ">"
+    
+    for k,v in data_set.items():
+        print(k, v)
+        fig.add_trace(
+            go.Scatter(x=timestamp, y=v, name=k, mode="lines+markers", line_shape='spline', connectgaps=True),
+            secondary_y=True,
+        )
+
+    for k,v in data_set.items():
+        fig.update_yaxes(title_text=y_label, secondary_y=True)
+
+
+    # label angle
+    fig.update_xaxes(tickangle = 45)
+    # update plat labels with titles
+    fig.update_layout(
+        title=plot_title,
+        xaxis_title=x_label,
+        yaxis_title=y_label,
+        legend_title="Legend"
+    )
+    # Write image to file
+    fig.write_image(image_file, width=900, height=600)
+
+# generate graph
+def generateGraphReport(reportFile, x_axis="", y_axis="", title="", description="", file_name=""):
     df1 = pd.read_csv(reportFile)
-    fig1 = px.line(df1, x=x_axis, y=y_axis, title=title)
+    labels = {}
+    labels['_'.join(x_axis.split())] = x_axis
+    labels['_'.join(y_axis.split())] = y_axis
+    fig1 = px.line(df1, x='_'.join(x_axis.split()), y='_'.join(y_axis.split()), title=title, labels=labels)
     fig1.write_image(file_name)
 
 # File path to store csv output
@@ -109,14 +188,13 @@ def writeToCsvFile(cmd_args, metrics):
         # write metrics to file
         print("Writing metrics to csv file: {}".format(file_name))
         with open(file_name, 'w') as csv_file:
-            fieldNames = ['SN', 'TIMESTAMP', 'METRIC_VALUE']
+            fieldNames = ['SN', 'TIMESTAMP', 'METRIC', 'GROUP']
             csvWriter = csv.DictWriter(csv_file, fieldnames=fieldNames)
-            csvWriter.writeheader() 
-            print("{},{}".format(fieldNames[0], fieldNames[1]))
+            csvWriter.writeheader()
+            print("{},{},{},{}".format(fieldNames[0], fieldNames[1], fieldNames[2], fieldNames[3]))
             for i in metrics:
                 row = i.split(',')
-                csvWriter.writerow({fieldNames[0]: row[0], fieldNames[1]: row[1], fieldNames[2]: row[2]})
-                #print("{},{},{}".format(row[0], row[1], row[2])) 
+                csvWriter.writerow({fieldNames[0]: row[0], fieldNames[1]: row[1], fieldNames[2]: row[2], fieldNames[3]: row[3]})
         return file_name
     else:
         print("Path is not a directory and/or does not exist: {}".format(file_path))
@@ -138,24 +216,38 @@ parser.add_argument("--timeout",  help="Prometheus HTTP request timeout", requir
 parser.add_argument("--file_path",  help="File path to store csv output", required=True)
 parser.add_argument("--file_name",  help="File name using workload/job name", required=True)
 parser.add_argument("--print",  help="Print", required=False)
+parser.add_argument("--x_axis",  help="x axis/label of graph", required=False)
+parser.add_argument("--y_axis",  help="y axis/lable of graph", required=False)
+parser.add_argument("--title",  help="Graph title", required=False)
+parser.add_argument("--description",  help="Graph description/info", required=False)
 args = parser.parse_args()
 
 # Main
 if __name__ == '__main__':
     # Get api url, http query params and auth credentials if exist
+    print(vars(args))
     url, params, basic_auth = createEndpoint(vars(args))
 
     # Send HTTP request and pull metrics from prometheus server
     metrics = getMetrics(url, params, basic_auth.get('user'), basic_auth.get('pass'))
+    for i in metrics:
+        print(i)
+        pass
 
     # Write metrics as csv data
     metrics_csv_file = writeToCsvFile(vars(args), metrics)
 
-    # Generate images
+    #Generate images
     print("CSV File: {}".format(metrics_csv_file))
     if os.path.isfile(metrics_csv_file):
-        file_path = vars(args).get('file_path')
-        file_name = vars(args).get('file_name').split('.', 1)[0] + '-graph.png'
+        cmd_params = vars(args)
+        title = cmd_params.get('title')
+        x_label = cmd_params.get('x_axis')
+        y_label = cmd_params.get('y_axis')
+        description = cmd_params.get('description')
+        file_path = cmd_params.get('file_path')
+        file_name = cmd_params.get('file_name').split('.', 1)[0] + '-graph.png'
         image_file = os.path.join(file_path, file_name)
         print("Image File: {}".format(image_file))
-        generateGraphReport(metrics_csv_file, title=file_name, file_name=image_file)
+        #generateGraphReport(metrics_csv_file, title=title, description=description, x_axis=x_axis, y_axis=y_axis, file_name=image_file)
+        plotGraph(metrics_csv_file, title, x_label, y_label, image_file)
